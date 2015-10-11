d61faa49511e9415b29b93ad69436303aeca1452
Lars <lars.scheme@gmail.com>
2015-10-11 21:57:45 +0200

    Added simple link creation using a batch file that will execute the destination path.

--- task/create_shortcut.go
+++ task/create_shortcut.go
@@ -1,19 +1,36 @@
 package task
 
-import "errors"
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
 
-func CreateShortcut(exePath, linkPath string) Task {
-	return &shortcut{exePath, linkPath}
+// CreateShortcut takes the exePath which is the destination file to execute
+// from the link, the linkPath which is the folder in which the link is to be
+// created, and the linkName which is the name the user sees for the link. It
+// must not contain an extension, the extension is automatically added by the
+// task.
+func CreateShortcut(exePath, linkPath, linkName string) Task {
+	return &shortcut{exePath, linkPath, linkName}
 }
 
 type shortcut struct {
-	exePath, linkPath string
+	exePath, linkPath, linkName string
 }
 
 func (t *shortcut) Name() string {
-	return "Creating shortcut to\n" + t.exePath + "\nin\n" + t.linkPath
+	return "Creating shortcut to\n" +
+		t.exePath + "\nin\n" +
+		t.linkPath +
+		"\nwith the name " + t.linkName
 }
 
 func (t *shortcut) Execute() error {
-	return errors.New("TODO create shortcut")
+	fileName := filepath.Join(t.linkPath, t.linkName+".bat")
+	exePath, exe := filepath.Split(t.exePath)
+	content := fmt.Sprintf(`start /d "%v" "%v"`, exePath, exe)
+	return makeError(
+		"creating link file '"+fileName+"'",
+		ioutil.WriteFile(fileName, []byte(content), 0666))
 }
